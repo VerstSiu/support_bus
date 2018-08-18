2df83d54de7352a9dff69c4ce78e78d1665e498c
Verst Siu <verstsiu@126.com>
2018-08-18 11:59:54 +0800

    :tada: Add first commit

---
+++ supportbus/src/main/java/com/ijoic/supportbus/SupportBus.kt
@@ -0,0 +1,66 @@
+/*
+ *
+ *  Copyright(c) 2018 VerstSiu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.ijoic.supportbus
+
+import android.arch.lifecycle.Lifecycle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+
+/**
+ * Support bus.
+ *
+ * @author verstsiu on 2018/8/18.
+ * @version 1.0
+ */
+object SupportBus {
+  /**
+   * Module items.
+   */
+  internal var moduleItems: List<SupportModule>? = null
+
+  /**
+   * Returns support bus config instance.
+   */
+  @JvmStatic
+  fun config() = SupportBusConfig()
+
+  /**
+   * Initialize activity.
+   *
+   * @param activity activity.
+   * @param lifecycle lifecycle.
+   */
+  @JvmStatic
+  @JvmOverloads
+  fun init(activity: FragmentActivity, lifecycle: Lifecycle? = null) {
+    moduleItems?.forEach { it.onActivityInit(activity, lifecycle ?: activity.lifecycle) }
+  }
+
+  /**
+   * Initialize fragment.
+   *
+   * @param fragment fragment.
+   * @param lifecycle lifecycle.
+   */
+  @JvmStatic
+  @JvmOverloads
+  fun init(fragment: Fragment, lifecycle: Lifecycle? = null) {
+    moduleItems?.forEach { it.onFragmentInit(fragment, lifecycle ?: fragment.lifecycle) }
+  }
+
+}
---
+++ supportbus/src/main/java/com/ijoic/supportbus/SupportModule.kt
@@ -0,0 +1,54 @@
+/*
+ *
+ *  Copyright(c) 2018 VerstSiu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.ijoic.supportbus
+
+import android.app.Activity
+import android.arch.lifecycle.Lifecycle
+import android.content.Context
+import android.support.v4.app.Fragment
+
+/**
+ * Support module.
+ *
+ * @author verstsiu on 2018/8/18.
+ * @version 1.0
+ */
+interface SupportModule {
+  /**
+   * Application init.
+   *
+   * @param context application context.
+   */
+  fun onAppInit(context: Context)
+
+  /**
+   * Activity init.
+   *
+   * @param activity activity.
+   * @param lifecycle lifecycle.
+   */
+  fun onActivityInit(activity: Activity, lifecycle: Lifecycle)
+
+  /**
+   * Fragment init.
+   *
+   * @param fragment fragment.
+   * @param lifecycle lifecycle.
+   */
+  fun onFragmentInit(fragment: Fragment, lifecycle: Lifecycle)
+}
